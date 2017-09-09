9c15d46a25e73baa1e30ac602ae1040454aacd1f
David Scrobonia <davidscrobonia@gmail.com>
2017-09-09 11:05:00 -0700

    finished BASIC refactor of all analyzers. Elliptic doesn't output anything, may be broken.

--- analyzers/response_size_meanshift.py
+++ analyzers/response_size_meanshift.py
@@ -1,178 +1,137 @@
 import sys
-from AccessLog import *
 import json
-
-import numpy as np
-
-
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
-import numpy as np
-
-import matplotlib.pyplot as plt
-
-from sklearn.cluster import MiniBatchKMeans, KMeans
-from sklearn.metrics.pairwise import pairwise_distances_argmin
-
-from sklearn.preprocessing import StandardScaler
-
-
-
-
-#Call toJson function from AccessLog.py to convert the log file "partial-log-july.txt" to
-#json format
-
-import sys
-data = toJson(sys.argv[1])
-
-
-#Convert this to python data for us to be able to run ML algorithms
-json_to_python = json.loads(data)
-
-
-##For logging to INFO.log and DEBUG.log:
+import re
+import urlparse
 import logging
 from logging.handlers import RotatingFileHandler
 
-logger_info = logging.getLogger('info_logger')
-logger_info.setLevel(logging.INFO)
-handler_info = RotatingFileHandler('INFO.log', mode = 'w',   backupCount=0)
-logger_info.addHandler(handler_info)
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sklearn.cluster import MiniBatchKMeans, KMeans
+from sklearn.metrics.pairwise import pairwise_distances_argmin
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import MeanShift, estimate_bandwidth
+from sklearn.datasets.samples_generator import make_blobs
+from itertools import cycle
 
 
+def analyze(data):
+    #Convert this to python data for us to be able to run ML algorithms
+    json_to_python = json.loads(data)
 
-logger_debug = logging.getLogger('debug_logger')
-logger_debug.setLevel(logging.INFO)
-handler_debug = RotatingFileHandler('DEBUG.log', mode = 'w',  backupCount=0)
-logger_debug.addHandler(handler_debug)
+    logger_info = logging.getLogger('info_logger')
+    logger_info.setLevel(logging.INFO)
+    handler_info = RotatingFileHandler('INFO.log', mode = 'w',   backupCount=0)
+    logger_info.addHandler(handler_info)
+
+    logger_debug = logging.getLogger('debug_logger')
+    logger_debug.setLevel(logging.INFO)
+    handler_debug = RotatingFileHandler('DEBUG.log', mode = 'w',  backupCount=0)
+    logger_debug.addHandler(handler_debug)
+
+    logger_attack = logging.getLogger('results_logger')
+    logger_attack.setLevel(logging.INFO)
+    handler_attack = RotatingFileHandler('ATTACK.log', mode = 'w',  backupCount=0)
+    logger_attack.addHandler(handler_attack)
+
+    per_size = dict() #IP-Response size
+    hostlist = dict()
+
+    #Data pre-processing here:
+    for i in json_to_python:
+
+        y = json_to_python[i] 
+        hostlist[y['HOST']] = 1
+
+        if y['HOST'] in per_size:
+            
+            per_size[y['HOST']].append(int(y['SIZE']))
+
+        else:
+            
+            per_size[y['HOST']] = [int(y['SIZE'])]
+
+     
+    ##Data pre-processing ends here
+
+    logger_debug.info("*** Printing Input to analysis - 4 (1): K-means on IP and average response size ****")
 
 
+    #####*****SIZE******####
+    #### Analysis #4 (1): IP address - Size of response received feature
+    X = np.array([[0.00,0.00]]) 
 
-logger_attack = logging.getLogger('results_logger')
-logger_attack.setLevel(logging.INFO)
-handler_attack = RotatingFileHandler('ATTACK.log', mode = 'w',  backupCount=0)
-logger_attack.addHandler(handler_attack)
-
-
-# Import re and urlparse for pre-processing
-import re
-import urlparse
-
-
-per_size = dict() #IP-Response size
-hostlist = dict()
-
-
-#Data pre-processing here:
-for i in json_to_python:
-
-    y = json_to_python[i] 
-    hostlist[y['HOST']] = 1
-
-    if y['HOST'] in per_size:
+    for x in hostlist:
         
-        per_size[y['HOST']].append(int(y['SIZE']))
+        avg_size = mean(per_size[x])
+        logger_debug.info( x + ": " + str(avg_size))
+        y = x.split(".")
+        ip = ""
+        for z in range(4):
+            l = len(y[z])
+            l = 3 - l
+            if(l>0):
+                zero = ""
+                for t in range(3 - len(y[z])):
+                    zero = zero + "0"
+                y[z] = zero + y[z]
 
-    else:
-        
-        per_size[y['HOST']] = [int(y['SIZE'])]
-
- 
-##Data pre-processing ends here
-
-logger_debug.info("*** Printing Input to analysis - 4 (1): K-means on IP and average response size ****")
+            ip = ip + y[z]
 
 
-#####*****SIZE******####
-#### Analysis #4 (1): IP address - Size of response received feature
-X = np.array([[0.00,0.00]]) 
+        #logger_debug.info( str(float(float(ip)/1000)) + ": " + str(avg_size))
+        le = [float(float(ip)/1000),avg_size]
+
+        X = np.vstack([X,le])
+
+
+    logger_attack.info( "********    Printing Analysis #4: IP-Address and Response Size received: MEAN SHIFT algorithm   ********")
+    logger_attack.info("Please check the graph at test-mean-shift.png for more info!")
+    #print kmeans.labels_
+
+
+    ## Analysis 4 (6): MEAN SHIFT algorithm: (IP-response size) #########
+    # #############################################################################
+    # Generate sample data
+    X1 = X
+
+    centers = X1
+
+    X, _ = make_blobs(n_samples=10000, centers=centers, cluster_std=0.6)
+
+    # #############################################################################
+    # Compute clustering with MeanShift
+
+    # The following bandwidth can be automatically detected using
+    bandwidth = estimate_bandwidth(X, quantile=0.2, n_samples=500)
+
+    ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
+    ms.fit(X)
+    labels = ms.labels_
+    cluster_centers = ms.cluster_centers_
+
+    labels_unique = np.unique(labels)
+    n_clusters_ = len(labels_unique)
+
+    logger_attack.info("number of estimated clusters : %d" % n_clusters_)
+
+    # #############################################################################
+    # Plot result
+    plt.figure(1)
+    plt.clf()
+
+    colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
+    for k, col in zip(range(n_clusters_), colors):
+        my_members = labels == k
+        cluster_center = cluster_centers[k]
+        plt.plot(X[my_members, 0], X[my_members, 1], col + '.')
+        plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
+                 markeredgecolor='k', markersize=14)
+    plt.title('Estimated number of clusters: %d' % n_clusters_)
+    ##plt.show()
+    plt.savefig('test-mean-shift.png')
 
 
 def mean(numbers):
     return float(sum(numbers)) / max(len(numbers), 1)
-
-for x in hostlist:
-    
-    avg_size = mean(per_size[x])
-    logger_debug.info( x + ": " + str(avg_size))
-    y = x.split(".")
-    ip = ""
-    for z in range(4):
-        l = len(y[z])
-        l = 3 - l
-        if(l>0):
-            zero = ""
-            for t in range(3 - len(y[z])):
-                zero = zero + "0"
-            y[z] = zero + y[z]
-
-        ip = ip + y[z]
-
-
-    #logger_debug.info( str(float(float(ip)/1000)) + ": " + str(avg_size))
-    le = [float(float(ip)/1000),avg_size]
-
-    X = np.vstack([X,le])
-
-
-logger_attack.info( "********    Printing Analysis #4: IP-Address and Response Size received: MEAN SHIFT algorithm   ********")
-logger_attack.info("Please check the graph at test-mean-shift.png for more info!")
-#print kmeans.labels_
-
-
-## Analysis 4 (6): MEAN SHIFT algorithm: (IP-response size) #########
-import numpy as np
-from sklearn.cluster import MeanShift, estimate_bandwidth
-from sklearn.datasets.samples_generator import make_blobs
-
-# #############################################################################
-# Generate sample data
-X1 = X
-
-import urlparse
-centers = X1
-
-X, _ = make_blobs(n_samples=10000, centers=centers, cluster_std=0.6)
-
-# #############################################################################
-# Compute clustering with MeanShift
-
-# The following bandwidth can be automatically detected using
-bandwidth = estimate_bandwidth(X, quantile=0.2, n_samples=500)
-
-ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
-ms.fit(X)
-labels = ms.labels_
-cluster_centers = ms.cluster_centers_
-
-labels_unique = np.unique(labels)
-n_clusters_ = len(labels_unique)
-
-logger_attack.info("number of estimated clusters : %d" % n_clusters_)
-
-# #############################################################################
-# Plot result
-import matplotlib.pyplot as plt
-from itertools import cycle
-
-plt.figure(1)
-plt.clf()
-
-colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
-for k, col in zip(range(n_clusters_), colors):
-    my_members = labels == k
-    cluster_center = cluster_centers[k]
-    plt.plot(X[my_members, 0], X[my_members, 1], col + '.')
-    plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
-             markeredgecolor='k', markersize=14)
-plt.title('Estimated number of clusters: %d' % n_clusters_)
-##plt.show()
-plt.savefig('test-mean-shift.png')
-
-
-
-
-
-
-
-
--- sawyer.py
+++ sawyer.py
@@ -1,6 +1,6 @@
 import argparse
 import logreader
-from analyzers import response_size_centroidmedian, response_size_dbscan, response_size_dendogramhac, response_size_lof, hourly_peak, hourly_requests_by_host, hourly_requests_to_server, param_count, param_extra, param_length, response_code
+import analyzers
 
 parser = argparse.ArgumentParser(description='Get more from your logs.')
 
@@ -19,8 +19,19 @@ parser.add_argument('--param-length', action='store_true')
 parser.add_argument('--response-code', action='store_true')
 parser.add_argument('--response-size-centroid', action='store_true')
 parser.add_argument('--response-size-dbscan', action='store_true')
+parser.add_argument('--response-size-elliptic', action='store_true')
 parser.add_argument('--response-size-hac', action='store_true')
+parser.add_argument('--response-size-kmeans', action='store_true')
+parser.add_argument('--response-size-knn', action='store_true')
 parser.add_argument('--response-size-local-outlier', action='store_true')
+parser.add_argument('--response-size-meanshift', action='store_true')
+parser.add_argument('--response-size-singlecomplete', action='store_true')
+parser.add_argument('--response-size-wardavg', action='store_true')
+parser.add_argument('--response-status', action='store_true')
+parser.add_argument('--url-requested', action='store_true')
+parser.add_argument('--url-requested-lots', action='store_true')
+parser.add_argument('--url-requested-once', action='store_true')
+parser.add_argument('--verb', action='store_true')
 
 args = parser.parse_args()
 
@@ -29,101 +40,93 @@ logs = logreader.toJson(args.logfile)
 
 # run analysis
 if args.all:
-	hourly_peak.analyze(logs)
-	hourly_requests_by_host.analyze(logs)
-	hourly_requests_to_server.analyze(logs)
-	param_count.analyze(logs)
-	param_extra.analyze(logs)
-	param_length.analyze(logs)
-	response_code.analyze(logs)
-	response_size_dbscan.analyze(logs)
-	response_size_dendogramhac.analyze(logs)
-	response_size_lof.analyze(logs)
-	response_size_centroidmedian.analyze(logs)
+	analyzers.hourly_peak.analyze(logs)
+	analyzers.hourly_requests_by_host.analyze(logs)
+	analyzers.hourly_requests_to_server.analyze(logs)
+	analyzers.param_count.analyze(logs)
+	analyzers.param_extra.analyze(logs)
+	analyzers.param_length.analyze(logs)
+	analyzers.response_code.analyze(logs)
+	analyzers.response_size_centroidmedian.analyze(logs)
+	analyzers.response_size_dbscan.analyze(logs)
+	analyzers.response_size_elliptic.analyze(logs)
+	analyzers.response_size_dendogramhac.analyze(logs)
+	analyzers.response_size_kmeans.analyze(logs)
+	analyzers.response_size_knn.analyze(logs)
+	analyzers.response_size_lof.analyze(logs)
+	analyzers.response_size_meanshift.analyze(logs)
+	analyzers.response_size_singlecomplete.analyze(logs)
+	analyzers.response_size_wardavg.analyze(logs)
+	analyzers.response_status.analyze(logs)
+	analyzers.url_requested.analyze(logs)
+	analyzers.url_requested_lots.analyze(logs)
+	analyzers.url_requested_once.analyze(logs)
+	analyzers.verb.analyze(logs)
 
 else:
 	if args.hourly_peak:
-		hourly_peak.analyze(logs)
+		analyzers.hourly_peak.analyze(logs)
 
 	if args.hourly_requests_by_host:
-		hourly_requests_by_host.analyze(logs)
+		analyzers.hourly_requests_by_host.analyze(logs)
 
 	if args.hourly_requests_to_server:
-		hourly_requests_to_server.analyze(logs)
+		analyzers.hourly_requests_to_server.analyze(logs)
 
 	if args.param_count:
-		param_count.analyze(logs)
+		analyzers.param_count.analyze(logs)
 
 	if args.param_extra:
-		param_extra.analyze(logs)
+		analyzers.param_extra.analyze(logs)
 
 	if args.param_length:
-		param_length.analyze(logs)
+		analyzers.param_length.analyze(logs)
 
 	if args.response_code:
-		response_code.analyze(logs)
+		analyzers.response_code.analyze(logs)
 
 	if args.response_size_dbscan:
-		response_size_dbscan.analyze(logs)
+		analyzers.response_size_dbscan.analyze(logs)
+
+	if args.response_size_elliptic:
+		#todo: no output
+		analyzers.response_size_elliptic.analyze(logs)
 
 	if args.response_size_hac:
-		response_size_dendogramhac.analyze(logs)
+		analyzers.response_size_dendogramhac.analyze(logs)
+
+	if args.response_size_kmeans:
+		analyzers.response_size_kmeans.analyze(logs)
+
+	if args.response_size_knn:
+		analyzers.response_size_knn.analyze(logs)
 
 	if args.response_size_local_outlier:
-		response_size_lof.analyze(logs)
+		analyzers.response_size_lof.analyze(logs)
 
 	if args.response_size_centroid:
-		response_size_centroidmedian.analyze(logs)
+		analyzers.response_size_centroidmedian.analyze(logs)
 
-'''
-elif args.elliptic:
-	response_size_elliptic.analyze(logs)
+	if args.response_size_meanshift:
+		analyzers.response_size_meanshift.analyze(logs)
 
-elif args.knn:
-	response_size_knn.analyze(logs)
+	if args.response_size_singlecomplete:
+		analyzers.response_size_singlecomplete.analyze(logs)
 
-elif args.meanshift:
-	print 'run meanshift'
+	if args.response_size_wardavg:
+		analyzers.response_size_wardavg.analyze(logs)
 
-elif args.centroid_median_hac:
-	print 'run centroid-median-hac'
+	if args.response_status:
+		analyzers.response_status.analyze(logs)
 
-elif args.single_complete_hac:
-	print 'run single-complete-hac'
+	if args.url_requested:
+		analyzers.url_requested.analyze(logs)
 
-elif args.ward_avg_hac:
-	print 'run ward-avg-hac'
+	if args.url_requested_lots:
+		analyzers.url_requested_lots.analyze(logs)
 
-elif args.response_size_kmeans:
-	print 'run response-size-kmeans'
+	if args.url_requested_once:
+		analyzers.url_requested_once.analyze(logs)
 
-elif args.response_status_kmeans:
-	print 'run response-status-kmeans'
-
-elif args.response_code:
-	print 'run response-code'
-
-elif args.verb_kproto:
-	print 'run verb-kproto'
-
-elif args.url_kproto:
-	print 'run url-kproto'
-
-elif args.num_param_ip:
-	print 'run num-param-ip'
-
-elif args.ip_url_param:
-	print 'run ip-url-params'
-
-elif args.url_one_time_hit:
-	print 'run url-one-time-hit'
-
-elif args.hosts_unique_url_hits:
-	print 'run hosts-unique-url-hits'
-
-elif args.missing_extra_params:
-	print 'run missing-extra-params'
-
-else:
-	print 'run all'
-'''
+	if args.verb:
+		analyzers.verb.analyze(logs)
