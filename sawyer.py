c2f11a5b740bb7a8bbf1224b36af9febe7904de7
jtmelton <jtmelton@gmail.com>
2017-09-12 02:21:36 -0400

    Rewrote to be more dynamic and not use direct references. This should ease adding new analyzers in the future (auto-registration) and it also requires a bit less code.

--- sawyer.py
+++ sawyer.py
@@ -1,132 +1,53 @@
 import argparse
-import logreader
+import sys
+
+# important to load analyzers
 import analyzers
+import logreader
 
-parser = argparse.ArgumentParser(description='Get more from your logs.')
 
-parser.add_argument('logfile', help='the web application logs to parse')
-parser.add_argument('-c', '--config', metavar='', help='a configuration file')
-parser.add_argument('-d', '--debug', metavar='', help='output debug information')
-parser.add_argument('-v', '--verbose', metavar='', help='output additional information')
+def main():
+    # list all available analyzers by name
+    available_analyzers = ['hourly_peak', 'hourly_requests_by_host', 'hourly_requests_to_server',
+                           'param_count', 'param_extra', 'param_length',
+                           'response_code', 'response_size_centroidmedian', 'response_size_dbscan',
+                           'response_size_elliptic', 'response_size_dendogramhac', 'response_size_kmeans',
+                           'response_size_knn', 'response_size_lof', 'response_size_meanshift',
+                           'response_size_singlecomplete', 'response_size_wardavg', 'response_status',
+                           'url_requested', 'url_requested_lots', 'url_requested_once', 'verb']
 
-parser.add_argument('-a', '--all', action='store_true', help='run all analyzers')
-parser.add_argument('--hourly-peak', action='store_true')
-parser.add_argument('--hourly-requests-by-host', action='store_true')
-parser.add_argument('--hourly-requests-to-server', action='store_true')
-parser.add_argument('--param-count', action='store_true')
-parser.add_argument('--param-extra', action='store_true')
-parser.add_argument('--param-length', action='store_true')
-parser.add_argument('--response-code', action='store_true')
-parser.add_argument('--response-size-centroid', action='store_true')
-parser.add_argument('--response-size-dbscan', action='store_true')
-parser.add_argument('--response-size-elliptic', action='store_true')
-parser.add_argument('--response-size-hac', action='store_true')
-parser.add_argument('--response-size-kmeans', action='store_true')
-parser.add_argument('--response-size-knn', action='store_true')
-parser.add_argument('--response-size-local-outlier', action='store_true')
-parser.add_argument('--response-size-meanshift', action='store_true')
-parser.add_argument('--response-size-singlecomplete', action='store_true')
-parser.add_argument('--response-size-wardavg', action='store_true')
-parser.add_argument('--response-status', action='store_true')
-parser.add_argument('--url-requested', action='store_true')
-parser.add_argument('--url-requested-lots', action='store_true')
-parser.add_argument('--url-requested-once', action='store_true')
-parser.add_argument('--verb', action='store_true')
+    parser = argparse.ArgumentParser(description='Get more from your logs.')
 
-args = parser.parse_args()
+    parser.add_argument('logfile', help='the web application log to parse')
+    parser.add_argument('-c', '--config', metavar='', help='a configuration file')
+    parser.add_argument('-d', '--debug', metavar='', help='output debug information')
+    parser.add_argument('-v', '--verbose', metavar='', help='output additional information')
 
-# read log file
-logs = logreader.toJson(args.logfile)
+    parser.add_argument('-a', '--all', action='store_true', help='run all analyzers')
 
-# run analysis
-if args.all:
-	analyzers.hourly_peak.analyze(logs)
-	analyzers.hourly_requests_by_host.analyze(logs)
-	analyzers.hourly_requests_to_server.analyze(logs)
-	analyzers.param_count.analyze(logs)
-	analyzers.param_extra.analyze(logs)
-	analyzers.param_length.analyze(logs)
-	analyzers.response_code.analyze(logs)
-	analyzers.response_size_centroidmedian.analyze(logs)
-	analyzers.response_size_dbscan.analyze(logs)
-	analyzers.response_size_elliptic.analyze(logs)
-	analyzers.response_size_dendogramhac.analyze(logs)
-	analyzers.response_size_kmeans.analyze(logs)
-	analyzers.response_size_knn.analyze(logs)
-	analyzers.response_size_lof.analyze(logs)
-	analyzers.response_size_meanshift.analyze(logs)
-	analyzers.response_size_singlecomplete.analyze(logs)
-	analyzers.response_size_wardavg.analyze(logs)
-	analyzers.response_status.analyze(logs)
-	analyzers.url_requested.analyze(logs)
-	analyzers.url_requested_lots.analyze(logs)
-	analyzers.url_requested_once.analyze(logs)
-	analyzers.verb.analyze(logs)
+    # add a cli switch for each analyzer
+    for analyzer in available_analyzers:
+        parser.add_argument('--' + analyzer, action='store_true')
 
-else:
-	if args.hourly_peak:
-		analyzers.hourly_peak.analyze(logs)
+    args = parser.parse_args()
 
-	if args.hourly_requests_by_host:
-		analyzers.hourly_requests_by_host.analyze(logs)
+    # read log file
+    log = logreader.toJson(args.logfile)
 
-	if args.hourly_requests_to_server:
-		analyzers.hourly_requests_to_server.analyze(logs)
+    # run analysis for all analyzers
+    if args.all:
+        for analyzer in available_analyzers:
+            # dynamically reference analyzer in global lookup
+            sys.modules['analyzers.' + analyzer].analyze(log)
 
-	if args.param_count:
-		analyzers.param_count.analyze(logs)
+    else:
+        for analyzer in available_analyzers:
+            # check for key in dictionary of args
+            if vars(args)[analyzer]:
+                print sys.modules.keys()
+                # dynamically reference analyzer in global lookup
+                sys.modules['analyzers.' + analyzer].analyze(log)
 
-	if args.param_extra:
-		analyzers.param_extra.analyze(logs)
 
-	if args.param_length:
-		analyzers.param_length.analyze(logs)
-
-	if args.response_code:
-		analyzers.response_code.analyze(logs)
-
-	if args.response_size_dbscan:
-		analyzers.response_size_dbscan.analyze(logs)
-
-	if args.response_size_elliptic:
-		#todo: no output
-		analyzers.response_size_elliptic.analyze(logs)
-
-	if args.response_size_hac:
-		analyzers.response_size_dendogramhac.analyze(logs)
-
-	if args.response_size_kmeans:
-		analyzers.response_size_kmeans.analyze(logs)
-
-	if args.response_size_knn:
-		analyzers.response_size_knn.analyze(logs)
-
-	if args.response_size_local_outlier:
-		analyzers.response_size_lof.analyze(logs)
-
-	if args.response_size_centroid:
-		analyzers.response_size_centroidmedian.analyze(logs)
-
-	if args.response_size_meanshift:
-		analyzers.response_size_meanshift.analyze(logs)
-
-	if args.response_size_singlecomplete:
-		analyzers.response_size_singlecomplete.analyze(logs)
-
-	if args.response_size_wardavg:
-		analyzers.response_size_wardavg.analyze(logs)
-
-	if args.response_status:
-		analyzers.response_status.analyze(logs)
-
-	if args.url_requested:
-		analyzers.url_requested.analyze(logs)
-
-	if args.url_requested_lots:
-		analyzers.url_requested_lots.analyze(logs)
-
-	if args.url_requested_once:
-		analyzers.url_requested_once.analyze(logs)
-
-	if args.verb:
-		analyzers.verb.analyze(logs)
+if __name__ == "__main__":
+    main()
